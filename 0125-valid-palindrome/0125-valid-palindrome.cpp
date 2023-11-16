7be6921924688c827392b06d18c2ad45033d2d25
Aditya Armal <mr.adityaarmal@gmail.com>
2023-11-17 01:15:48 +0530

    Time: 4 ms (80.63%), Space: 7.6 MB (62.15%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !std::isalnum(s[left])) {
+                ++left;
+            }
+            while (left < right && !std::isalnum(s[right])) {
+                --right;
+            }
+            if (std::tolower(s[left]) != std::tolower(s[right])) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+};
