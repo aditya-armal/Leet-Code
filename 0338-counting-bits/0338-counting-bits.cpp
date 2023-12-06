663c05f8a11ac53bb93d20ca93021216d17c5a36
Aditya Armal <mr.adityaarmal@gmail.com>
2023-12-06 23:03:18 +0530

    Time: 6 ms (47.79%), Space: 8.3 MB (52.72%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        std::vector<int> result(n + 1, 0);
+
+        for (int i = 1; i <= n; ++i) {
+            result[i] = result[i >> 1] + (i & 1);
+        }
+
+        return result;
+    }
+};
