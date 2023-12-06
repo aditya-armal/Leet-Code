c741b4c811b3770aa37a0a7db05d0462c8c55242
Aditya Armal <mr.adityaarmal@gmail.com>
2023-12-06 23:04:20 +0530

    Time: 5 ms (9.87%), Space: 6.5 MB (22.73%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0 || (n & (n - 1)) != 0) {
+            return false;
+        }
+        return (n & 0x55555555) != 0;
+    }
+};
