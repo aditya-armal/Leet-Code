2a9250890561c687c58a25c08fcb5018385e47c7
Aditya Armal <mr.adityaarmal@gmail.com>
2023-12-09 05:11:43 +0530

    Time: 60 ms (49.51%), Space: 44.6 MB (46.33%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentValue = init;
+
+    return {
+        increment: function() {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrement: function() {
+            currentValue -= 1;
+            return currentValue;
+        },
+        reset: function() {
+            currentValue = init;
+            return currentValue;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
