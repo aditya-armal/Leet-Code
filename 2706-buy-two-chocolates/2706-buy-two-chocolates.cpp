bc626c8ef1050fe6e44311884f5b9eec5184806a
Aditya Armal <mr.adityaarmal@gmail.com>
2023-12-27 03:50:10 +0530

    Time: 18 ms (24.03%), Space: 46.9 MB (96.79%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+    int minCost = INT_MAX;
+
+        int n = prices.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int cost = prices[i] + prices[j];
+
+                if (cost < minCost) {
+                    minCost = cost;
+                }
+            }
+        }
+
+        if (minCost <= money) {
+            return money - minCost;
+        } else {
+            return money;
+        }
+    }
+};
